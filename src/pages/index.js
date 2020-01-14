76a3690dce2672199584bd452d9675a3d4064131
teerathkumar2000 <teerathukrani999@gmail.com>
2020-01-14 22:25:36 +0500

    media and gallery

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import style from '../styles/index.module.scss'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+import Introduction from './introduction'
 
 const IndexPage = () => (
 <div className={style.maindiv}>
